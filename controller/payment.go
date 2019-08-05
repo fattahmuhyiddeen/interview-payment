a86a216fc641b13186df1ea8e49bf38674adff0b
fattahmuhyiddeen <fattahmuhyiddeen@gmail.com>
2019-08-06 00:49:40 +0800

    bug fix

--- controller/payment.go
+++ controller/payment.go
@@ -1,28 +1,25 @@
 package controller
 
 import (
-	"net/http"
 	"math/rand"
+	"net/http"
+
 	"github.com/labstack/echo"
 )
 
 //PaymentStatusResponse is to construct response object for payment status
 type PaymentStatusResponse struct {
 	Status string `json:"status"`
-	Code int `json:"code"`
+	Code   int    `json:"code"`
 }
 
 var paymentStatuses = []string{
-    "fail",
-    "success",
+	"fail",
+	"success",
 }
 
 //PaymentStatus return payment status
 func PaymentStatus(c echo.Context) (err error) {
-	index := rand.Intn(2)
-	response := PaymentStatusResponse{
-		Status: paymentStatuses[index],
-		Code: index,
-	}
+	response := rand.Intn(2)
 	return c.JSON(http.StatusOK, response)
 }
